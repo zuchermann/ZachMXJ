0651efb3fc39e9f6c556e65362e1b0fd66b1372b
Lamtharn Hantrakul <Hanoi@lawn-128-61-116-89.lawn.gatech.edu>
2016-11-18 17:55:34 -0500

    HI Zach, I added a test

---
+++ src/HanoiStuff/test_1_withzk.java
@@ -0,0 +1,14 @@
+package HanoiStuff;
+
+/**
+ * Created by Hanoi on 11/18/16.
+ */
+
+import com.cycling74.max.*;
+
+public class test_1_withzk extends MaxObject {
+
+    public void bang() {
+        post("hello world");
+    }
+}

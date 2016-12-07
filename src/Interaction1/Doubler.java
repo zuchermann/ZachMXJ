5d0db08e7b62d8bc1bf5830918932fd2846934e4
Lamtharn Hantrakul <Hanoi@lawn-128-61-119-14.lawn.gatech.edu>
2016-12-07 16:47:41 -0500

    doubler

---
+++ src/Interaction1/Doubler.java
@@ -0,0 +1,35 @@
+package Interaction1;
+
+import com.cycling74.max.*;
+
+/**
+ * Created by Hanoi on 12/7/16.
+ */
+public class Doubler extends MaxObject{
+    double minVal;
+    double MIN = 20;
+
+    public Doubler(){
+        this.minVal = 500;
+
+        createInfoOutlet(false);
+
+        declareInlets(new int[]{ DataTypes.ALL });
+        declareOutlets(new int[]{ DataTypes.ALL });
+    }
+
+    public void inlet(float val){
+        if(val > MIN) {
+            double result = val;
+            while (result < minVal) {
+                result = result * 2.;
+            }
+            outlet(0, result);
+        }
+    }
+
+    public void setLatency(double minVal){
+        this.minVal = minVal;
+        post("doubler set to " + minVal);
+    }
+}
